53cd2d22289d4f070512896679588b58128f96d4
Aryan Tyagi <aryantyagi296@gmail.com>
2024-08-22 21:18:27 +0530

    added user schema

--- models/User.js
+++ models/User.js
@@ -1,1 +1,25 @@
 const mongoose = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        fullName: { type: String, default: '' },
+        adhaarNumber: { type: String, required: true },
+        password: { type: String, required: true },
+        age: { type: String, required: true },
+        userRole: { type: String, default: 'Voter', enum: ['Voter', 'Admin', 'Candidate'] },
+        createdAt: { type: Date },
+        updatedAt: { type: Date },
+    },
+    {
+        timestamps: true
+    }
+);
+
+userSchema.pre('save', function(next){
+    this.updated_at = Date.now();
+    next();
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
